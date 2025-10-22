07d2cbe3571f4b3076fc6fcef9f47ff2e841fa8c
Artem <alavr011@uottawa.ca>
2025-10-22 18:57:54 -0400

    Frontend prototype of NBA App

--- src/Tabs/Goal_Tab.jsx
+++
@@ -1,9 +0,0 @@
-function Goal_Tab() {
-  return (
-    <div>
-      <h1 className='title'>Goals</h1>
-    </div>
-  );
-}
-
-export default Goal_Tab;
--- src/Tabs/Main_Tab.jsx
+++
@@ -1,9 +0,0 @@
-function Main_Tab() {
-  return (
-    <div>
-      <h1 className='title'>Main Tab</h1>
-    </div>
-  );
-}
-
-export default Main_Tab;
