4a25e148920e4c36a7c13a702d386b6fb16b8876
ArturMigulea <artur.migulea@gmail.com>
2025-10-30 11:34:21 -0400

    Update main.jsx

--- BallDontLie/src/main.jsx
+++ BallDontLie/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+const port = process.env.PORT || 3001;
+App.listen(port, () => console.log(`NBA proxy running on :${port}`));
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
